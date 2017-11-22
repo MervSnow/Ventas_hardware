62b3f236402be7f9b9f9a2bb4478a8e2ded1115a
Mervs <angeuchiha@gmail.com>
2017-11-22 09:56:04 -0600

    Add files via upload

---
+++ Venta_hardware/frmInicio.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Venta_hardware
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblUsr = new System.Windows.Forms.Label();
+            this.lblPasw = new System.Windows.Forms.Label();
+            this.txtUsu = new System.Windows.Forms.TextBox();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(1, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 17);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblTitulo);
+            this.panel2.Location = new System.Drawing.Point(1, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(683, 60);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel3.Location = new System.Drawing.Point(350, 60);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(334, 17);
+            this.panel3.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Lucida Console", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lblTitulo.Location = new System.Drawing.Point(222, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(236, 27);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Iniciar Sesion";
+            // 
+            // lblUsr
+            // 
+            this.lblUsr.AutoSize = true;
+            this.lblUsr.Font = new System.Drawing.Font("Lucida Console", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsr.ForeColor = System.Drawing.Color.Black;
+            this.lblUsr.Location = new System.Drawing.Point(171, 153);
+            this.lblUsr.Name = "lblUsr";
+            this.lblUsr.Size = new System.Drawing.Size(140, 27);
+            this.lblUsr.TabIndex = 0;
+            this.lblUsr.Text = "Usuario:";
+            // 
+            // lblPasw
+            // 
+            this.lblPasw.AutoSize = true;
+            this.lblPasw.Font = new System.Drawing.Font("Lucida Console", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasw.ForeColor = System.Drawing.Color.Black;
+            this.lblPasw.Location = new System.Drawing.Point(123, 238);
+            this.lblPasw.Name = "lblPasw";
+            this.lblPasw.Size = new System.Drawing.Size(188, 27);
+            this.lblPasw.TabIndex = 0;
+            this.lblPasw.Text = "Contraseña:";
+            // 
+            // txtUsu
+            // 
+            this.txtUsu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsu.Location = new System.Drawing.Point(312, 153);
+            this.txtUsu.Name = "txtUsu";
+            this.txtUsu.Size = new System.Drawing.Size(187, 31);
+            this.txtUsu.TabIndex = 2;
+            // 
+            // txtContra
+            // 
+            this.txtContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContra.Location = new System.Drawing.Point(312, 236);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.PasswordChar = '*';
+            this.txtContra.Size = new System.Drawing.Size(187, 31);
+            this.txtContra.TabIndex = 2;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.txtContra);
+            this.Controls.Add(this.txtUsu);
+            this.Controls.Add(this.lblPasw);
+            this.Controls.Add(this.lblUsr);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio Sesion";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblUsr;
+        private System.Windows.Forms.Label lblPasw;
+        private System.Windows.Forms.TextBox txtUsu;
+        private System.Windows.Forms.TextBox txtContra;
+    }
+}
+
